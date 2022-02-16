ed402d6b3f67dd64f1de2f8a32525faaa01a276e
Viktor Kim <viktor.kim@clicklogiq.com>
2022-02-16 23:31:07 +0200

    added bootstrap, remove initial code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header>header</header>
+      <main>content</main>
+      <footer>footer</footer>
+    </>
+  );
+}
+
+export default App;
